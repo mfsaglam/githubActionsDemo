052d0f4c646e6f435fcdb1d2baee9aa16614b84a
mfsaglam <mfsaglam1@gmail.com>
2025-03-20 00:55:12 +0300

    Initial Commit

---
+++ GithubActionsDemo/GithubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsDemoApp.swift
+//  GithubActionsDemo
+//
+//  Created by Fatih Sağlam on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
