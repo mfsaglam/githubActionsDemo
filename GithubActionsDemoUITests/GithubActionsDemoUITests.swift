0110795a86054c1d97a80954c9c3f973416159b9
Fatih Saglam <m.saglam@enbw.com>
2025-05-02 16:41:15 +0300

    Add basic UI test and integrate UI tests into CI workflow

--- GithubActionsDemoUITests/GithubActionsDemoUITests.swift
+++ GithubActionsDemoUITests/GithubActionsDemoUITests.swift
@@ -27,15 +27,7 @@ final class GithubActionsDemoUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
+        XCTAssertEqual(2*2, 4)
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
